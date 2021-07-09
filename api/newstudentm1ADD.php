5c41d023cb395a00e9c7ed7a8456c615cc273ad7
worng1234 <himax34529@gmail.com>
2021-07-09 11:52:03 +0700

    fix file

--- api/newstudentm1ADD.php
+++ api/newstudentm1ADD.php
@@ -13,10 +13,12 @@
 
     $msg['message'] = '';
 
-    $sql = "INSERT INTO new_student_register_m1(prename,m1_name,surname,sex,pic,id_number,birthday,religion,nationality,
-    origin,father_name,father_id,father_job,father_tel) 
-    VALUES(:prename, :m1_name, :surname, :sex, :pic, :id_number, :birthday, :religion, :nationality, :origin,
-    :father_name, :father_id, :father_job, :father_tel)";
+    $sql = "INSERT INTO new_student_register_m1(prename,m1_name,surname,sex,pic,id_number,birthday,religion,
+    nationality,origin,house_number,bloc,street,road,sub_district,district,province,post,final_school,
+    final_school_sub_district,final_school_district,final_school_province,m1_disabled,poor_person,etc,tel,email,name_cen) 
+    VALUES(:prename,:m1_name,:surname,:sex,:pic,:id_number,:birthday,:religion,:nationality,:origin,
+    :house_number,:bloc,:street,:road,:sub_district,:district,:province,post,:final_school,
+    :final_school_sub_district,:final_school_district,:final_school_province,:m1_disabled,:etc,:email,:name_cen)";
     
     $stmt = $conn->prepare($sql); 
 
@@ -30,19 +32,6 @@
     $stmt->bindValue(':religion', htmlspecialchars_decode(strip_tags($data->religion)), PDO::PARAM_STR);
     $stmt->bindValue(':nationality', htmlspecialchars_decode(strip_tags($data->nationality)), PDO::PARAM_STR);
     $stmt->bindValue(':origin', htmlspecialchars_decode(strip_tags($data->origin)), PDO::PARAM_STR);
-    $stmt->bindValue(':father_name', htmlspecialchars_decode(strip_tags($data->father_name)), PDO::PARAM_STR);
-    $stmt->bindValue(':father_id', htmlspecialchars_decode(strip_tags($data->father_id)), PDO::PARAM_STR);
-    $stmt->bindValue(':father_job', htmlspecialchars_decode(strip_tags($data->father_job)), PDO::PARAM_STR);
-    $stmt->bindValue(':father_tel', htmlspecialchars_decode(strip_tags($data->father_tel)), PDO::PARAM_STR);
-    /*$stmt->bindValue(':mother_name', htmlspecialchars_decode(strip_tags($data->mother_name)), PDO::PARAM_STR);
-    $stmt->bindValue(':mother_id', htmlspecialchars_decode(strip_tags($data->mother_id)), PDO::PARAM_STR);
-    $stmt->bindValue(':mother_job', htmlspecialchars_decode(strip_tags($data->mother_job)), PDO::PARAM_STR);
-    $stmt->bindValue(':mother_tel', htmlspecialchars_decode(strip_tags($data->mother_tel)), PDO::PARAM_STR);
-    $stmt->bindValue(':parent', htmlspecialchars_decode(strip_tags($data->parent)), PDO::PARAM_STR);
-    $stmt->bindValue(':parent_name', htmlspecialchars_decode(strip_tags($data->parent_name)), PDO::PARAM_STR);
-    $stmt->bindValue(':parent_id', htmlspecialchars_decode(strip_tags($data->parent_id)), PDO::PARAM_STR);
-    $stmt->bindValue(':parent_job', htmlspecialchars_decode(strip_tags($data->parent_job)), PDO::PARAM_STR);
-    $stmt->bindValue(':parent_tel', htmlspecialchars_decode(strip_tags($data->parent_tel)), PDO::PARAM_STR);
     $stmt->bindValue(':house_number', htmlspecialchars_decode(strip_tags($data->house_number)), PDO::PARAM_STR);
     $stmt->bindValue(':bloc', htmlspecialchars_decode(strip_tags($data->bloc)), PDO::PARAM_STR);
     $stmt->bindValue(':street', htmlspecialchars_decode(strip_tags($data->street)), PDO::PARAM_STR);
@@ -61,13 +50,7 @@
     $stmt->bindValue(':tel', htmlspecialchars_decode(strip_tags($data->tel)), PDO::PARAM_STR);
     $stmt->bindValue(':email', htmlspecialchars_decode(strip_tags($data->email)), PDO::PARAM_STR);
     $stmt->bindValue(':name_cen', htmlspecialchars_decode(strip_tags($data->name_cen)), PDO::PARAM_STR);
-    $stmt->bindValue(':father_namecen', htmlspecialchars_decode(strip_tags($data->father_namecen)), PDO::PARAM_STR);
-    $stmt->bindValue(':mother_namecen', htmlspecialchars_decode(strip_tags($data->mother_namecen)), PDO::PARAM_STR);
-    $stmt->bindValue(':parent_namecen', htmlspecialchars_decode(strip_tags($data->parent_namecen)), PDO::PARAM_STR);
-    $stmt->bindValue(':father_surname', htmlspecialchars_decode(strip_tags($data->father_surname)), PDO::PARAM_STR);
-    $stmt->bindValue(':mother_surname', htmlspecialchars_decode(strip_tags($data->mother_surname)), PDO::PARAM_STR);
-    $stmt->bindValue(':parent_surname', htmlspecialchars_decode(strip_tags($data->parent_surname)), PDO::PARAM_STR);
-    */
+    
     if($stmt->execute()){
         $msg['message'] = 'Data Inserted Successfully';
     }
