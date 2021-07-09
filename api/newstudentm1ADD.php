92da146e85dcac8e51e009aed9e5bca79f2d0cd8
worng1234 <himax34529@gmail.com>
2021-07-09 15:24:03 +0700

    fix file

--- api/newstudentm1ADD.php
+++ api/newstudentm1ADD.php
@@ -13,12 +13,10 @@
 
     $msg['message'] = '';
 
-    $sql = "INSERT INTO new_student_register_m1(prename,m1_name,surname,sex,pic,id_number,birthday,religion,
-    nationality,origin,house_number,bloc,street,road,sub_district,district,province,post,final_school,
-    final_school_sub_district,final_school_district,final_school_province,m1_disabled,poor_person,etc,tel,email,name_cen) 
+    $sql = "INSERT INTO `new_student_register_m1`(prename,m1_name,surname,sex,pic,id_number,birthday,religion,
+    nationality,origin,m1_disabled,poor_person,etc,tel,email,name_cen) 
     VALUES(:prename,:m1_name,:surname,:sex,:pic,:id_number,:birthday,:religion,:nationality,:origin,
-    :house_number,:bloc,:street,:road,:sub_district,:district,:province,post,:final_school,
-    :final_school_sub_district,:final_school_district,:final_school_province,:m1_disabled,:etc,:email,:name_cen)";
+    :m1_disabled,:poor_person,:etc,:tel,:email,:name_cen)";
     
     $stmt = $conn->prepare($sql); 
 
@@ -32,19 +30,7 @@
     $stmt->bindValue(':religion', htmlspecialchars_decode(strip_tags($data->religion)), PDO::PARAM_STR);
     $stmt->bindValue(':nationality', htmlspecialchars_decode(strip_tags($data->nationality)), PDO::PARAM_STR);
     $stmt->bindValue(':origin', htmlspecialchars_decode(strip_tags($data->origin)), PDO::PARAM_STR);
-    $stmt->bindValue(':house_number', htmlspecialchars_decode(strip_tags($data->house_number)), PDO::PARAM_STR);
-    $stmt->bindValue(':bloc', htmlspecialchars_decode(strip_tags($data->bloc)), PDO::PARAM_STR);
-    $stmt->bindValue(':street', htmlspecialchars_decode(strip_tags($data->street)), PDO::PARAM_STR);
-    $stmt->bindValue(':road', htmlspecialchars_decode(strip_tags($data->road)), PDO::PARAM_STR);
-    $stmt->bindValue(':sub_district', htmlspecialchars_decode(strip_tags($data->sub_district)), PDO::PARAM_STR);
-    $stmt->bindValue(':district', htmlspecialchars_decode(strip_tags($data->district)), PDO::PARAM_STR);
-    $stmt->bindValue(':province', htmlspecialchars_decode(strip_tags($data->province)), PDO::PARAM_STR);
-    $stmt->bindValue(':post', htmlspecialchars_decode(strip_tags($data->post)), PDO::PARAM_STR);
-    $stmt->bindValue(':final_school', htmlspecialchars_decode(strip_tags($data->final_school)), PDO::PARAM_STR);
-    $stmt->bindValue(':final_school_sub_district', htmlspecialchars_decode(strip_tags($data->final_school_sub_district)), PDO::PARAM_STR);
-    $stmt->bindValue(':final_school_district', htmlspecialchars_decode(strip_tags($data->final_school_district)), PDO::PARAM_STR);
-    $stmt->bindValue(':final_school_province', htmlspecialchars_decode(strip_tags($data->final_school_province)), PDO::PARAM_STR);
-    $stmt->bindValue(':m1_diabled', htmlspecialchars_decode(strip_tags($data->m1_disabled)), PDO::PARAM_STR);
+    $stmt->bindValue(':m1_disabled', htmlspecialchars_decode(strip_tags($data->m1_disabled)), PDO::PARAM_STR);
     $stmt->bindValue(':poor_person', htmlspecialchars_decode(strip_tags($data->poor_person)), PDO::PARAM_STR);
     $stmt->bindValue(':etc', htmlspecialchars_decode(strip_tags($data->etc)), PDO::PARAM_STR);
     $stmt->bindValue(':tel', htmlspecialchars_decode(strip_tags($data->tel)), PDO::PARAM_STR);
